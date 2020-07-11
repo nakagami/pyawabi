25834ff51792e1cf729b4e734dcc661cc898089e
Hajime Nakagami <nakagami@gmail.com>
2020-07-11 09:37:52 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,35 @@
+import sys
+
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
+
+try:
+    from setuptools_rust import RustExtension
+except ImportError:
+    import subprocess
+
+    errno = subprocess.call([sys.executable, "-m", "pip", "install", "setuptools-rust"])
+    if errno:
+        print("Please install setuptools-rust package")
+        raise SystemExit(errno)
+    else:
+        from setuptools_rust import RustExtension
+
+setup_requires = ["setuptools-rust>=0.10.1", "wheel"]
+install_requires = []
 
 setup(
     name="pyawabi",
-    version="0.2",
-    rust_extensions=[RustExtension("pyawabi.awabi", binding=Binding.PyO3)],
+    version="0.2.0",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+    ],
     packages=["pyawabi"],
+    rust_extensions=[RustExtension("pyawabi.awabi")],
+    setup_requires=setup_requires,
     zip_safe=False,
 )
