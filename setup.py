84f95779af2159b3f9712f42a7a92e6e514f4c4b
Hajime Nakagami <nakagami@gmail.com>
2020-12-08 12:59:08 +0900

    fix fail to install windows platform

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ install_requires = []
 
 setup(
     name="pyawabi",
-    version="0.2.3",
+    version="0.2.4",
     description='A morphological analyzer using mecab dictionary.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     url='http://github.com/nakagami/pyawabi/',
     classifiers=[
